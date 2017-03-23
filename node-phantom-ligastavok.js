0d9cf7553db30e4ff3ca6f6d3c08074e5b2c48a4
Unknown <bo4arovpavel1989@mail.ru>
2017-03-23 14:59:41 +0300

    123

---
+++ node-phantom-ligastavok.js
@@ -0,0 +1,44 @@
+console.log('running app');
+var cheerio = require('cheerio');
+var phantom = require("phantom");
+var _ph, _page, _outObj;
+
+function getMarja(){
+	phantom.create().then(ph => {
+		_ph = ph;
+		return _ph.createPage();
+	}).then(page => {
+		_page = page;
+		return _page.open('https://www.ligastavok.ru/Live', { charset: 'utf-8'});
+	}).then(status => {
+		//console.log(status);
+		return _page.property('content')
+	}).then(content => {
+		let $ = cheerio.load(content);
+
+		console.log($('.ob.ov.typeWIN').html());
+		/*let home = $('.liverow>.templOdds.home>.val').html();
+		let draw=$('.liverow>.templOdds.draw>.val').html();
+		let away=$('.liverow>.templOdds.away>.val').html();
+		let marja=0;
+		if(!isNaN(Number(home)) && (Number(home))!==0) marja += 100/Number(home);
+		if(!isNaN(Number(draw)) && (Number(draw))!==0) marja += 100/Number(draw);
+		if(!isNaN(Number(away)) && (Number(away))!==0) marja += 100/Number(away);
+		marja = marja - 100;
+		console.log('live event marja - ' + marja);
+		home = $('.prematches.home>.val').html();
+		draw=$('.prematches.draw>.val').html();
+		away=$('.prematches.away>.val').html();
+		marja = 0;
+		if(!isNaN(Number(home)) && (Number(home))!==0) marja += 100/Number(home);
+		if(!isNaN(Number(draw))&& (Number(draw))!==0) marja += 100/Number(draw);
+		if(!isNaN(Number(away))&& (Number(away))!==0) marja += 100/Number(away);
+		marja = marja - 100;
+		console.log('prematch marja - ' + marja);*/
+		_page.close();
+		_ph.exit();
+	}).catch(e => console.log(e));
+}
+
+getMarja();
+//setInterval(function(){getMarja()}, 5000);
