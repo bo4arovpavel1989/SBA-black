1fdcff92e0d19d6333e7c6f90aa0f9e0d0aea13d
bo4arovpavel1989 <bo4arovpavel1989@mail.ru>
2017-03-26 12:42:34 +0300

    Parsers leon front page uncluding type of sport

--- node-phantom-leon.js
+++ node-phantom-leon.js
@@ -18,6 +18,9 @@ function getMarja(){
 		
 		let tdLive=$('.liverow>.templOdds.home>.val').get();
 		tdLive.forEach((tdElem)=>{
+			let sport = tdElem.parent.prev.prev.prev.prev.prev.children[4].children[0].data;
+			sport = sport.split(' - ');
+			sport = sport[0];
 			let home = tdElem.children[0].data;
 			let draw = tdElem.parent.next.next.next.next.children[1].children[0].data;
 			let away = tdElem.parent.next.next.next.next.next.next.next.next.children[1].children[0].data;	
@@ -26,21 +29,24 @@ function getMarja(){
 			if(!isNaN(Number(draw)) && (Number(draw))!==0) marja += 100/Number(draw);
 			if(!isNaN(Number(away)) && (Number(away))!==0) marja += 100/Number(away);
 			marja = marja - 100;
-			console.log('liveevent coeffs: ' + home + '-' + draw + '-' + away + '. Marja: ' + marja + '\n');	
+			console.log('liveevent coeffs: ' + sport + ' - ' + home + '-' + draw + '-' + away + '. Marja: ' + marja + '\n');
 		});
 		
 		let tdPrematch=$('.prematches.home>.val').get();
 		tdPrematch.forEach((tdElem)=>{
+			let sport = tdElem.parent.prev.prev.prev.children[5].children[0].data;
+			sport = sport.split(' - ');
+			sport = sport[0];
+			sport = sport.slice(17);
 			let home = tdElem.children[0].data;
 			let draw = tdElem.parent.next.next.children[1].children[0].data;;
 			let away = tdElem.parent.next.next.next.next.children[1].children[0].data;	
-			
 			let marja=0;
 			if(!isNaN(Number(home)) && (Number(home))!==0) marja += 100/Number(home);
 			if(!isNaN(Number(draw)) && (Number(draw))!==0) marja += 100/Number(draw);
 			if(!isNaN(Number(away)) && (Number(away))!==0) marja += 100/Number(away);
 			marja = marja - 100;
-			console.log('prematch coeffs: ' + home + '-' + draw + '-' + away + '. Marja - ' + marja + '\n');
+			console.log('prematch coeffs: ' + sport  + ' - ' + home + '-' + draw + '-' + away + '. Marja - ' + marja + '\n');
 		});
 		
 		_page.close();
