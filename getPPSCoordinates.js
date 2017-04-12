b57034e8de18728e6765f8dc07346f126f33bcea
bo4arovpavel1989 <bo4arovpavel1989@mail.ru>
2017-04-12 22:56:18 +0300

    started getting coordinates

--- getPPSCoordinates.js
+++ getPPSCoordinates.js
@@ -9,18 +9,12 @@ String.prototype.replaceAll = function(search, replace){
 BkPPS.find({}, function(err, rep){
 	let address=rep[0].address;
 	address = address.replaceAll(', ', '+');
-	
-	address='Санкт-Петербург+улица+Рыбацкая+д.10+кв.2';
+	//address=address.slice(0, 80);
 	console.log(address);
+	 address=encodeURIComponent(address); 
 	let query='https://maps.googleapis.com/maps/api/geocode/json?address=' + address + '&key=' + secret;
-	  var options = {
-        host :  'maps.googleapis.com',
-        port : 443,
-        path : '/maps/api/geocode/json?address=' + address + '&key=' + secret,
-        method : 'GET'
-    }
 	console.log(query);
-	  https.get('https://maps.googleapis.com/maps/api/geocode/json?address=%D0%A1%D0%B0%D0%BD%D0%BA%D1%82-%D0%9F%D0%B5%D1%82%D0%B5%D1%80%D0%B1%D1%83%D1%80%D0%B3+%D1%83%D0%BB%D0%B8%D1%86%D0%B0+%D1%80%D1%8B%D0%B1%D0%B0%D1%86%D0%BA%D0%B0%D1%8F+%D0%B4.10&key=AIzaSyBkhoB5RgaYw19-QWiFDoUc2AtTO-Sc2P0', function(res) {
+	  https.get(query, function(res) {
         var data = "";
         res.on('data', function (chunk) {
                         data += chunk.toString();
